281cac5e0a3c7fb38524e48410d492ff7efab122
Omer Baflah <aldonramos@gmail.com>
2021-11-12 23:56:17 +0300

    Add office migration

---
+++ database/migrations/2021_11_12_200647_create_offices_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOfficesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offices', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('lat',11,8);
+            $table->decimal('lng',11,8);
+            $table->text('address_line1');
+            $table->text('address_line2')->nullable();
+            $table->tinyInteger('approval_status')->default(1);
+            $table->boolean('hidden')->default(false);
+            $table->integer('price_per_day');
+            $table->integer('monthly_discount')->default(0);
+            $table->foreignId('user_id')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('offices');
+    }
+}
