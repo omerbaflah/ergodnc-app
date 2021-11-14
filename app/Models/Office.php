0f26793560baf8592daa0c23407ef49886275175
Omer Baflah <aldonramos@gmail.com>
2021-11-14 22:25:28 +0300

    Prepare relations in office model

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Office extends Model
 {
     use HasFactory;
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function images(): MorphMany
+    {
+        return $this->morphMany(Image::class,'resource');
+    }
 }
