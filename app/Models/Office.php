77c8bf35fe300a89872026024399e8618f35f76b
Omer Baflah <aldonramos@gmail.com>
2021-11-14 22:29:04 +0300

    Add casts attributes & use soft deletes in office model

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -8,10 +8,21 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Office extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $casts = [
+        'lat' => 'decimal:8',
+        'lng' => 'decimal:8',
+        'approval_status' => 'integer',
+        'hidden' => 'bool',
+        'price_per_day' => 'integer',
+        'monthly_discount' => 'integer',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
