1716d7f959c163a829b8bde1eaf2cb6474b591e5
Omer Baflah <aldonramos@gmail.com>
2021-11-16 00:05:28 +0300

    Add offices endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\OfficeController;
+use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('tags',TagController::class);
+
+Route::apiResource('offices', OfficeController::class)->except(['create','edit','destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
