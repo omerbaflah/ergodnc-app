0c83d38fe9197e39b202d89321bcf5b115f7c2dc
Omer Baflah <aldonramos@gmail.com>
2021-11-19 22:11:24 +0300

    Refactor to use constants as a default values in offices migration

--- database/migrations/2021_11_12_200647_create_offices_table.php
+++ database/migrations/2021_11_12_200647_create_offices_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Office;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -21,8 +22,8 @@ class CreateOfficesTable extends Migration
             $table->decimal('lng',11,8);
             $table->text('address_line1');
             $table->text('address_line2')->nullable();
-            $table->tinyInteger('approval_status')->default(1);
-            $table->boolean('hidden')->default(false);
+            $table->tinyInteger('approval_status')->default(Office::APPROVAL_APPROVED);
+            $table->boolean('hidden')->default(Office::VISIBLE);
             $table->integer('price_per_day');
             $table->integer('monthly_discount')->default(0);
             $table->foreignId('user_id')->index();
