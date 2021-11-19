ef7d0059230c4fcd1046e452de00cac492180ef6
Omer Baflah <aldonramos@gmail.com>
2021-11-19 19:54:54 +0300

    Eager load tags & images & user relations in offices list endpoint

--- app/Http/Resources/OfficeResource.php
+++ app/Http/Resources/OfficeResource.php
@@ -25,7 +25,9 @@ class OfficeResource extends JsonResource
             'hidden' => $this->hidden,
             'price_per_day' => $this->price_per_day,
             'monthly_discount' => $this->monthly_discount,
-            'user' => UsersListResource::make($this->user)
+            'user' => UsersListResource::make($this->user),
+            'images' => ImageResource::collection($this->images),
+            'tags' => TagResource::collection($this->tags),
         ];
     }
 }
