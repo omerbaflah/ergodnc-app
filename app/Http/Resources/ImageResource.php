0c3e68713c8b5135d9dcdb3a25bc188018a936b7
Omer Baflah <aldonramos@gmail.com>
2021-11-21 23:34:43 +0300

    Update image API resource

--- app/Http/Resources/ImageResource.php
+++ app/Http/Resources/ImageResource.php
@@ -14,6 +14,9 @@ class ImageResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'path' => $this->path
+        ];
     }
 }
